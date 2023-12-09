4557d9a21e5fd7367a2da0e035250ef73f02dd44
Žan Žerak <zan.zerak99@gmail.com>
2023-12-09 16:08:04 +0100

    added player movement logic

--- body_generation.py
+++ body_generation.py
@@ -2,6 +2,11 @@ from turtle import Turtle
 
 _PLAYER_MOVE_AMOUNT = 100
 _ENEMY_MOVE_AMOUNT = 50
+_MOVE_LEFT = 180
+_MOVE_RIGHT = 0
+_SET_HEAD_NORTH = 90
+_PLAYER_STARTING_Y_POSITION = -400
+_PLAYER_STARTING_X_POSITION = 0
 
 
 class Game_body():
@@ -9,18 +14,15 @@ class Game_body():
     new_body = None
     
     def __init__(self) -> None:
-            # self.shape = shape
-            # self.color = color
-            # self.generate_body(shape, color)
+        
             pass
-       
-
-      
-    # TODO:
-    # 1. Make a class method for body
+    
     def generate_body(self,shape,color):
             self.new_body = Turtle(shape)
+            self.new_body.penup()
             self.new_body.color(color) 
+            self.new_body.setheading(_SET_HEAD_NORTH)
+            self.new_body.setposition(_PLAYER_STARTING_X_POSITION,_PLAYER_STARTING_Y_POSITION)
            
           
     # 2. Make a class method for color
@@ -45,13 +47,16 @@ class User_body(Game_body):
     # 2. Make the only posible movement left and right for now
     
     def user_movment_left(self):
-        print("d")
-        self.new_body.left(180)
-        self.new_body.forward(100)
+        
+        self.new_body.setheading(_MOVE_LEFT)
+        self.new_body.forward(_PLAYER_MOVE_AMOUNT)
+        self.new_body.setheading(_SET_HEAD_NORTH)
     
     def user_movment_right(self):
-        self.new_body.right(180)
-        self.new_body.forward(100)
+        
+        self.new_body.setheading(_MOVE_RIGHT)
+        self.new_body.forward(_PLAYER_MOVE_AMOUNT)
+        self.new_body.setheading(_SET_HEAD_NORTH)
        
     
     # 2. Make a class method for shooting
